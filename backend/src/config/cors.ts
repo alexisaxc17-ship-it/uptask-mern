6f13d55ce6e4598949af5e5d8abf7b08e7842175
Alexis <alexisaxc@gmail.com>
2026-04-24 18:41:10 -0600

    Fix CORS config

--- backend/src/config/cors.ts
+++ backend/src/config/cors.ts
@@ -1,13 +1,20 @@
 import { CorsOptions } from "cors"
 
+const whitelist = [
+  process.env.FRONTEND_URL?.trim(),
+  "https://uptask-mern-ten.vercel.app"
+]
+
 export const corsConfig: CorsOptions = {
   origin: function (origin, callback) {
-    const whitelist = [process.env.FRONTEND_URL]
-
     if (!origin || whitelist.includes(origin)) {
       callback(null, true)
-    } else {
-      callback(new Error("Error de CORS"))
+      return
     }
+
+    console.log("Origen bloqueado por CORS:", origin)
+    console.log("Whitelist:", whitelist)
+
+    callback(new Error("Error de CORS"))
   }
 }
